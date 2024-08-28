539226d294036a72f007ce64d834845bb4e6129a
Abhishek Bhatta <abhishekbhatta110@gmail.com>
2024-08-28 23:52:16 +0545

    New project add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import One from "./Component/One"
-import React from 'react'
+import  {useState} from 'react'
 
 function App() {
+  const[Color,setColor] = useState("black");
   return (
-    <div>
-      <One/>
+    <div className="w-full duration-200 h-screen flex flex-wrap justify-center" style={{backgroundColor:Color}}>
+        <div className='bg-white p-2  mx-40 flex flex-wrap absolute top-85 bottom-20 rounded-lg gap-2'>
+      <button onClick={() => setColor("red") } className='bg-red-500 p-2 pl-5 pr-5 rounded-lg font-bold '>Red</button>
+      <button onClick={() => setColor("Green") } className='bg-green-300 p-2 pl-5 pr-5 rounded-lg font-bold '>Green</button>
+      <button onClick={() => setColor("Blue") } className='bg-blue-950 p-2 pl-5 pr-5 rounded-lg font-bold '>Blue</button>
+      <button  onClick={() => setColor("Purple") } className='bg-purple-800 p-2 pl-5 pr-5 rounded-lg font-bold '>Purple</button>
+      <button onClick={() => setColor("Yellow") } className='bg-yellow-300 p-2 pl-5 pr-5 rounded-lg font-bold '>Yellow</button>
+      <button onClick={() => setColor("Orange") } className='bg-orange-500 p-2 pl-5 pr-5 rounded-lg font-bold '>Orange</button>
+      <button onClick={() => setColor("pink") } className='bg-pink-500 p-2 pl-5 pr-5 rounded-lg font-bold '>Pink</button>
+     
+
+        </div>
     </div>
   )
 }
