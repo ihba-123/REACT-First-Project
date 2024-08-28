ef475dea7e9063196c8976851a358de40bff8fa7
Abhishek Bhatta <abhishekbhatta110@gmail.com>
2024-08-28 21:40:05 +0545

    First step

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import One from "./Component/One"
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <One/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/One.jsx
@@ -0,0 +1,9 @@
+function One() {
+  return (
+    <div>
+     <h1 className="text-red-500">Abhishek Bhatta</h1> 
+    </div>
+  )
+}
+
+export default One
